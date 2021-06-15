cb3d654b2a9631e71387180c0380d22462b56c3d
cristonal araneda <macair@MacBook-Air-de-Air.local>
2021-06-15 13:44:28 -0400

    Se implementa las grafica horzontal , instalación npm install @swimlane/ngx-charts --save y npm install @angular/cdk --save , diseña tabla de votación de juegos   con intervalo de 3 segundo de actualización de resultados

--- src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
+++ src/app/components/grafico-barra-horizontal/grafico-barra-horizontal.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-grafico-barra-horizontal',
   templateUrl: './grafico-barra-horizontal.component.html',
   styleUrls: ['./grafico-barra-horizontal.component.css']
 })
-export class GraficoBarraHorizontalComponent implements OnInit {
+export class GraficoBarraHorizontalComponent implements OnDestroy {
 
-  constructor() { }
+  result: any[]= [
+    {
+      "name": "Juego 1",
+      "value": 8940000
+    },
+    {
+      "name": "Juego2",
+      "value": 5000000
+    },
+    {
+      "name": "juego3",
+      "value": 7200000
+    },
+    {
+      "name": "juego4",
+      "value": 8700000
+    }
+  ];
+  
 
-  ngOnInit(): void {
+  
+  // options
+  showXAxis = true;
+  showYAxis = true;
+  gradient = true;
+  showLegend = true;
+  showXAxisLabel = true;
+  xAxisLabel = 'Juegos';
+  showYAxisLabel = true;
+  yAxisLabel = 'Votos';
+
+  colorScheme = 'nightLights';
+
+  intervalo;
+
+  constructor(){
+
+   this.intervalo = setInterval(()=>{
+      console.log('tick');
+
+      const newResult = [...this.result];
+
+      for (let i  in newResult ) {
+        newResult[i].value = Math.round( Math.random() * 500)
+        
+      }
+
+      this.result=[...newResult];
+
+    }, 3600);
+
+
+  }
+
+  onSelect(event) {
+    console.log(event);
+  }
+
+  ngOnDestroy(){
+    clearInterval( this.intervalo );
+    
   }
 
 }
